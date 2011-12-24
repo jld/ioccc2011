6026a20031a3030477bddf30c2dac99c4ae58b37
Jed Davis <jld@panix.com>
2011-12-23 21:47:31 -0800

    Reformat for prettiness

--- obfusat.c
+++ obfusat.c
@@ -30,20 +30,22 @@ int main()
 	scanf("p cnf %d %*d", &n);
 	i = calloc(2*n+2, sizeof* N);
 	for (i += n; n > 0; --n) { C x S L x C L x X L i[n] S U i[-n] S }
+
 	L N U c S p S rb: A+=1 == scanf("%d", &n) Z re;
 	L i L i+n Z ce U T S L c C c S Z rb; ce:
 	L c C L p C p S L N; c S Z rb; re:
+
 	L p C p S o S C w S pa: L p L c S U U Z sat;
 	I(c, x S l S I(l, U Z lb U Z cb) L x U U Z oa U Z as);
-	L o L p Z bees; o S Z pa;
-as: L T U L o S W cb;
-oa: L M U Z unsat; T S L H Z ob F H S F X D S Z oa;
-ob: L M F M S L w U L D W pa;
-bees: L p U U T S U U D S L w Y L H L M C M S L w U U L D W pa;
-unsat: printf("s UNSATISFIABLE\n"); return 0;
-sat: printf("s SATISFIABLE\nv ");
+	L o L p Z bees; o S Z pa; as: L T U L o S W cb; oa: 
+	L M U Z unsat; T S L H Z ob F H S F X D S Z oa; ob:
+	L M F M S L w U L D W pa; bees: 
+	L p U U T S U U D S L w Y L H L M C M S L w U U L D W pa;
+
+unsat:	printf("s UNSATISFIABLE\n"); return 0;
+sat:	printf("s SATISFIABLE\nv ");
 	for (n = 1; i[n]; ++n) { L i[n] U Z ub U Z uc; continue;
-uc: printf("%d ", n); if (0) ub: printf("%d ", -n);
+uc:		printf("%d ", n); if (0) ub: printf("%d ", -n);
 	} printf ("0\n");
 	return 0;
 }
