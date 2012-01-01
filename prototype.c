4b1e9fb3bcee2b8cfb480f43185bf587fbbf9f7f
Jed Davis <jld@panix.com>
2012-01-01 13:43:45 -0800

    Test also the dots

--- prototype.c
+++ prototype.c
@@ -4,6 +4,7 @@
 typedef void **t;
 void *N[] = {N, N};
 t G, H, M=N, p=N;
+char h;
 
 #define Z ; if (A == D) goto
 #define C ; *(T =++ G) =D ++; D -= (G =* -- T) ||! (G = calloc(2, sizeof* N)); \
@@ -22,7 +23,7 @@ t G, H, M=N, p=N;
 #define E(n) ; main(n, 0);
 #define R    ; return  0 ;
 #define T(B) B B B
-#define Q &n ["0111112011138777764877667586677758767675999:"]
+#define Q &n ["0111112011138777764877667586677758767675999:=>=?=<"]
 #define V(n,N,O) ; if (n Q) { N ; } else { O ; }
 
 void prn(t T, int l)
@@ -51,19 +52,24 @@ int main(int n, char **v)
 
 	T=A=D=N F C if (v) { E(0) L G U L a S Z s; }
 	V(7, L G U V(12, F y S,; ) x S,; );
-	V(8, V(2, L z S L p C p S
-		I(z, a S L z U x S I(x, U L N C L a U L T C
-			/*!*/ L N U L T C L p C p S)),
-		V(1, C F T(E(n+1)),
-		    L G U E(7) L G U x S L T L x C C F E(n+1)
-		    L x C F T(E(40)) R)),
+	V(8, V(4, L x L y C T S V(2, h='0',; )
+		V(3, C F T(E(n+1)) L G U, h+=h>'/') U
+		V(2, V(1, A+=h<'/'; h='.', h='\n'), V(1, A++,; ))
+		Z pr; rp:,
+		V(2, L z S L p C p S
+		    I(z, a S L z U x S I(x, U L N C L a U L T C
+			    /*!*/ L N U L T C L p C p S)),
+		    V(1, C F T(E(n+1)),
+			L G U E(7) L G U x S L T L x C C F E(n+1)
+			L x C F T(E(40)) R))),
 	    V(4, T(L y V(1, U y S,; ) L x U x S L T V(2, C C F E(n+1),
 			a S L a U L T U L T C L a X)) R,
 		V(2, V(1, /*!*/ L N L N C, C a S L a C L a X),
 		    L N C F T(E(n+1)) L G U)));
-	V(3, V(8, L y, L x C) C F,; ) R;
+	V(7, V(8, L y, L x C) C F,; ) R;
+pr:     putchar(h); h='.' Z rp;
 
-s:      E(12) C F E(19) C F E(26) C F E(33);
+s:      E(12) C F E(19) C F E(26) C F E(33) C F T(E(44));
 
 	L p C p S y S C z S pa: L p L c S U U Z sat;
 	I(c, x S l S I(l, U Z lb U Z cb) L x U U Z oa U Z as);
@@ -72,6 +78,5 @@ s:      E(12) C F E(19) C F E(26) C F E(33);
 	L M F M S L z U L D W pa; bees: 
 	L p U U T S U U D S L z Y L H L M C M S L z U U L D W pa;
 
-unsat:	printf("s UNSATISFIABLE\n") R;
-sat:	printf("s SATISFIABLE\n") R;
+sat:	L a C F T(E(44)) unsat: R;
 }
