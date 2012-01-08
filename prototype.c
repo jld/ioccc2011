44c8267cace066a23ceeda88348eacd4d28695fd
Jed Davis <jld@panix.com>
2012-01-07 20:37:47 -0800

    Densify a little.

--- prototype.c
+++ prototype.c
@@ -32,10 +32,9 @@ char h;
 int main(int n, char **v)
 {
 	int b, d;
-	t a, c, l, q, x, y, z;
-	t T, A, D;
+	t a, c, l, q, x, y, z, T, A, D;
 
-	T=A=D=N F C if (v) { v[1]&&(srandom(atoi(v[1])),0); E(0) L G U L a S Z s; }
+	T=A=D=N F C if (v) { v[1]&&(srandom(atoi(v[1])),0) Z s; }
 	V(7, L G U V(12, F y S,; ) x S,; );
 	V(8, V(4, L x L y C T S V(2, h='0',; )
 		V(3, C F T(E(n+1)) L G U, h+=h>'/') U
@@ -43,30 +42,29 @@ int main(int n, char **v)
 		Z pr; rp:,
 		V(2, L z S L p C p S
 		    I(z, a S L z U x S I(x, U L N C L a U L T C
-			    /*!*/ L N U L T C L p C p S)),
+			    L T C L p C p S)),
 		    V(1, C F T(E(n+1)),
 			L G U E(7) L G U x S L T L x C C F E(n+1)
 			L x C F T(E(40)) R))),
 	    V(4, T(L y V(1, U y S,; ) L x U x S L T V(2, C C F E(n+1),
 			a S L a U L T U L T C L a X)) R,
-		V(2, V(1, /*!*/ L N L N C, L N U C a S L H L a C L a X),
+		V(2, V(1, L N C, L N U C a S L H L a C L a X),
 		    L N C F T(E(n+1)) L G U)));
 	V(7, V(8, L y, L x C) C F,; ) R;
 pr:     putchar(h); h='.' Z rp;
 aa:     J(v) Z va; vc: d+=(b=1) Z pa;
 
-s:      E(12) C F E(19) C F E(26) C F E(33) /* C F T(E(44))*/;
+s:      E(0) L G U L a S E(12) C F E(19) C F E(26) C F E(33);
 
-	L p C L p S y S C z S pb: b=d=1; pa: n=1; /*printf("go ")*/ L p;
+	L p C L p S y S b=d=1 C z S pa: n=1; L p;
 	c S I(c, x S l S I(l, U Z lb U Z cb
 		L A+!(random()%n++) Z ld; q S q S ld:) L x U U Z oa U Z as);
 	L p U U Z sat L y L p Z bees; y S Z pa; as: L T U L y S W cb;
-	J(o) A+=(b-=d+1)>-1 Z aa L z U L D W pa; bees: /*printf("bees %d\n", n);*/
-	b+=d L q L A;
+	J(o) A+=(b-=d+1)>-1 Z aa L z U L D W pa; bees: b+=d L q L A;
 	U D S L N Z ya; L T L A Z yb; ya: L T U yb: L z Y
 	L H L M C M S L z U U L D W pa;
 
-sat:	/* printf("done\n") L a C F T(E(44)) */ L N;
+sat:	L N;
 	y S J(w) L z U L y C y S Z wa; wc:
 	L y F Z wd; y S U L D W wc; wd:
 	C L a C F T(E(44)) oc: R;
