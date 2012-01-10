8a405b49d0ef077e9279a48d0dec017f795bde40
Jed Davis <jld@panix.com>
2012-01-09 23:48:08 -0800

    Crossport the label-squashing I thought I'd already pulled

--- final.c
+++ final.c
@@ -18,11 +18,11 @@ int h;
 
 #define Y X C L H C H S
 #define W U D S L T Y Z
-#define I(P,B) P##a: L P U U Z P##c; D S B L P U P S Z P##a; \
-               P##b: L P U U L P Y Z P##a; P##c:
+#define I(P,B) P: L P U U Z P##b; D S B L P U P S Z P; \
+               P##a: L P U U L P Y Z P; P##b:
 
-#define J(P) P##a: L M U Z P##c; T S L H Z P##b F H S F X D S Z P##a; \
-             P##b: L M F M S
+#define J(P) P: L M U Z P##b; T S L H Z P##a F H S F X D S Z P; \
+             P##a: L M F M S
 
 #define E(m) ; n += main(m, 0);
 #define R    ; return  0 ;
@@ -43,8 +43,8 @@ int main(int n, char **v)
 		V(2, V(1, A+=h<47; h=46, h=10), V(1, A++,; ))
 		Z p; q:,
 		V(2, L z S L u C u S
-		    I(z, w S L z U x S I(x, U L N C Z xb L w U L T C
-			    L T C Z zb L u C u S)),
+		    I(z, w S L z U x S I(x, U L N C Z xa L w U L T C
+			    L T C Z za L u C u S)),
 		    V(1, C F T(E(n+1)),
 			L G U E(7) L G U x S L T L x C C F E(n+1)
 			L x C F T(E(40)) R))),
@@ -54,20 +54,20 @@ int main(int n, char **v)
 		    L N C F T(E(n+1)) L G U)));
 	V(7, V(8, L y, L x C) C F,; ) R;
 p:     (void)putchar(h); h=46 Z q;
-a:     J(v) Z va; vc: d+=(b=1) Z t;
+a:     J(v) Z v; vb: d+=(b=1) Z t;
 
 e:      E(0) L G U L w S E(12) C F E(19) C F E(26) C F E(33);
 
 	L u C L u S y S b=d=1 C z S t: n=1 L u;
-	r S I(r, x S s S I(s, U Z sb U Z rb
-		L A+!(random()%n++) Z sd; q S q S sd:) L x U U Z ua U Z o);
-	L u U U Z w L y L u Z b; y S Z t; o: L T U L y S W rb;
+	r S I(r, x S s S I(s, U Z sa U Z ra
+		L A+!(random()%n++) Z sc; q S q S sc:) L x U U Z u U Z o);
+	L u U U Z w L y L u Z b; y S Z t; o: L T U L y S W ra;
 	J(u) A+=(b-=d+1)>-1 Z a L z U L D W t; b: b+=d L q L A;
 	U D S L N Z g; L T L A Z h; g: L T U h: L z Y
 	L H L M C M S L z U U L D W t;
 
 w:	L N;
-	y S J(y) L z U L y C y S Z ya; yc:
-	L y F Z yd; y S U L D W yc; yd:
-	C L w C F T(E(44)) uc: R;
+	y S J(y) L z U L y C y S Z y; yb:
+	L y F Z yc; y S U L D W yb; yc:
+	C L w C F T(E(44)) ub: R;
 }
