14e78073446663cafad75a05b8c59f78ecbadac9
Jed Davis <jld@panix.com>
2012-01-10 21:36:01 -0800

    Art up the spacing a little more

--- final.c
+++ final.c
@@ -33,7 +33,7 @@ U   ,u=    A,Z    =A;        int   h;   int   main        (int    n   ,char   *
 * v)  { int   b,  d =        n;  t  q,  r,  s,  w,        x,  y,  z,  G,  N,  Q
 ;  G  =N=  Q=A  S  q=        w  =x=  y=z  =U  P  I        (96   *!   v  ,I(  7,
 Y  T  H  I( 12 ,S y F        ,;  )  x    F  ,;  ),        srandom    ((   unsi\
-gned   )(   v[    1]?        atoi   (v   [1    ]):        time(    0)))   M   e
+gned    )(   v[   1]?        atoi   (v   [1    ]):        time(    0)))   M   e
 ;       p:(     void)        putchar   (h   );  h=        46  M  q)  I(  8,  I(
 4 ,Y  x Y  y P G F I(        2,  h=   48   ,;  )I(        3  ,P  S G  (R (n  +1
 ))  Y  T  H  ,h += (h        >  47  )+  7  *!(  h-        57  ))  H  I(  2,  I(
@@ -43,9 +43,9 @@ gned   )(   v[    1]?        atoi   (v   [1    ]):        time(    0)))   M   e
 1,  N  +=  h<  47;  h        =  46,  h=  10  ),  I        (1   ,N    ++,;   ))M
 p  ; q  :, I( 2,  Y z        F Y  u P u  F V (z ,w        F Y  z H x  F V (x ,H
 Y A P M  xa Y w H Y G        P Y G P M  za Y u P u        F  )),  I(  1,  P S G
-(R  (n  +1  ))  ,Y  T        H R  (7) Y T H x  F Y        G Y  x P P  S R (n +1
+(R  (n   +1  ))  ,Y T        H R  (7) Y T H x  F Y        G Y  x P P  S R (n +1
 ) Y  x P S G  (R  (40        ))E    )))   ,I(   4,        G(Y  y I(  1, H  y  F
-,; )Y  x H x  F Y G I        (2,  P P  S R  (n  +1        ), w F Y  w H Y G H Y
+,; )Y  x H x  F Y G I        (2,  P P  S  R (n  +1        ), w F Y  w H Y G H Y
 G P Y  w K ))  E  ,I(        2  ,I(  1 ,Y  A  P ,Y        A H P w F Y U Y w P Y
 w K ), Y  A P S G  (R        (n  +1  ))  Y  T  H))        )  I(  7, I(  8, Y  y
 , Y  x  P)P  S ,;  )E        ; e  :R (0  )Y  T H Y        w  F R(  12  )P  S R(
@@ -55,9 +55,9 @@ w K ), Y  A P S G  (R        (n  +1  ))  Y  T  H))        )  I(  7, I(  8, Y  y
 19  )P  S  R(  26  )P        S  R( 33 )Y  u P Y  u        F y  F b =d= 1 P z  F
 t : n = 1 Y u ; r F V        (r  ,x  F s  F  V (s,        H M  sa H M  ra Y  N+
 !(     random     ()%        n  ++)M  sc  ; q  F q        F sc  :) Y  x H H M u
-H M o) Y u H H M  w Y        y Y u M b ; y F M t ;        a  :W  (v  )M  v;  vb
+H M o )Y  u H H M w Y        y Y u M b ; y F M t ;        a  :W  (v  )M  v;  vb
 :d  +=  (b  =1)  M  t        ; o : Y  G H Y  y F J        ra; W  (u  )N  +=(  b
--=d  +1  )>  -1  M  a        Y z H Y Q J t ; b : b        +=  d Y q Y N H Q F Y
+-=d  +1  )>  -1   M a        Y z H Y Q J t ; b : b        += d Y q Y N H Q  F Y
 A M g Y G Y N M h ; g        : Y G H h : Y z L Y U        Y Z P Z F Y z H H Y Q
 J t ; w : Y A ; y F W        ( y ) Y z H Y y P y F        M y ; yb : Y y S M y\
-c ; y  F H Y Q J yb ;        yc  : P Y  w P S G (R        ( 44  ))  ub  : E ; }
+c ; y F H Y  Q J yb ;        yc  : P Y  w P S G (R        ( 44  ))  ub  : E ; }
